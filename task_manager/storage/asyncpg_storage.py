19ebc6d2963b5f5aad6171d0c49fc711188dd98e
codefather.ua <codefather.ua@icloud.com>
2024-03-06 16:28:39 +0200

    easy refactoring

--- task_manager/storage/asyncpg_storage.py
+++ task_manager/storage/asyncpg_storage.py
@@ -15,6 +15,14 @@ from task_manager.core.storage import StorageInterface, OnTaskCallback, Transact
 class AsyncPGStorage(StorageInterface):
     def __init__(self):
         self.pool: Optional[Pool] = None
+        self.subscribe_conn: Optional[Connection] = None
+        self.connection_data = {
+            "user": os.environ.get("POSTGRES_USER", "postgres"),
+            "password": os.environ.get("POSTGRES_PASS", "task_manager_password"),
+            "database": os.environ.get("POSTGRES_NAME", "task_manager"),
+            "host": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+            "port": os.environ.get("POSTGRES_PORT", "5432"),
+        }
         self.lock = asyncio.Lock()
         self.tasks = []  # todo postgres impl
 
@@ -23,18 +31,19 @@ class AsyncPGStorage(StorageInterface):
         self.on_task_callbacks = []
 
     async def create_connection(self):
-        if not self.pool or self.pool._closed:
+        if not self.pool or self.pool.is_closing():
             self.pool = await asyncpg.create_pool(
-                user=os.environ.get("POSTGRES_USER", "postgres"),
-                password=os.environ.get("POSTGRES_PASS", "task_manager_password"),
-                database=os.environ.get("POSTGRES_NAME", "task_manager"),
-                host=os.environ.get("POSTGRES_HOST", "127.0.0.1"),
-                port=os.environ.get("POSTGRES_PORT", "5432"),
+                **self.connection_data,
                 max_size=10
+            )
+        if not self.subscribe_conn or self.subscribe_conn.is_closed():
+            self.subscribe_conn = await asyncpg.connect(
+                **self.connection_data
             )
 
     async def close_connection(self):
-        await self.pool.close()
+        await self.pool.close() if self.pool else None
+        await self.subscribe_conn.close() if self.subscribe_conn else None
 
     async def create_task(self, queue, payload) -> str:
         """
@@ -54,10 +63,12 @@ class AsyncPGStorage(StorageInterface):
             ")"
 
         async with self.pool.acquire() as connection:
+            connection: Connection
             async with connection.transaction():
                 await connection.fetch(q)
 
-        self.tasks.append(task)
+        # FIXME what is this ?
+        # self.tasks.append(task)
 
         # todo postgres impl
         for callback in self.on_task_callbacks:
@@ -82,17 +93,19 @@ class AsyncPGStorage(StorageInterface):
              f"WHERE (idn='{idn}' and status={NEW}) RETURNING * ;")
 
         async with self.pool.acquire() as connection:
-            async with connection.transaction():
-                res: list | asyncpg.Record = await connection.fetch(q)
+            connection: Connection
 
-        if res:
-            res: asyncpg.Record = res[0]
-            task = Task.from_dict(dict(res))
+            async with connection.transaction():
+                rec: asyncpg.Record = await connection.fetchrow(q)
+
+        if rec:
+            task = Task.from_dict(dict(rec))
             return ConsumedTaskResult(
                 task,
                 self.lock
             )
 
+        # TODO is this legal? (release only without rec flow)
         self.lock.release()
         return None
 
@@ -117,17 +130,19 @@ class AsyncPGStorage(StorageInterface):
         )
 
         async with self.pool.acquire() as connection:
-            async with connection.transaction():
-                res: list | asyncpg.Record = await connection.fetch(q)
+            connection: Connection
 
-        if res:
-            res: asyncpg.Record = res[0]
-            task = Task.from_dict(dict(res))
+            async with connection.transaction():
+                rec: asyncpg.Record = await connection.fetchrow(q)
+
+        if rec:
+            task = Task.from_dict(dict(rec))
             return ConsumedTaskResult(
                 task,
                 self.lock
             )
 
+        # TODO is this legal? (release only without rec flow)
         self.lock.release()
         return None
 
@@ -142,13 +157,20 @@ class AsyncPGStorage(StorageInterface):
             f"RETURNING *;"
         )
         async with self.pool.acquire() as connection:
-            async with connection.transaction():
-                res: list | asyncpg.Record = await connection.fetch(q)
+            connection: Connection
 
-        if not res:
+            async with connection.transaction():
+                rec: asyncpg.Record = await connection.fetchrow(q)
+
+        if not rec:
             # todo: add exceptions inheritance level to interface and raise appropriate one
             raise Exception(f"Can't finish task. Task '{idn}' not found in storage")
 
-    def add_on_task_callback(self, callback: OnTaskCallback):
+    def add_on_task_callback(self, callback: OnTaskCallback, channel: str = 'tasks'):
         # todo postgres impl
         self.on_task_callbacks.append(callback)
+
+        # self.subscribe_conn.add_listener(
+        #     channel=channel,
+        #     callback=callback
+        # )
